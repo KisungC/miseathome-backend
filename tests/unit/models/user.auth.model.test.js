c5070b93400bc22ab0357c4399081fa63cd5453a
kisungc <chung.kisung0@gmail.com>
2025-07-21 21:39:16 -0400

    added createUser test case

--- tests/unit/models/user.auth.model.test.js
+++ tests/unit/models/user.auth.model.test.js
@@ -2,12 +2,13 @@ jest.mock('../../../database')
 jest.mock('../../../models/user.model')
 
 const {createUser, findByEmail, findByUsername} = require('../../../models/user.model')
-const {mockUser} = require('../../utils/factories')
+const {mockUser, mockCreateUserRes} = require('../../utils/factories/mockUser')
+const { mockUserInput } = require('../../utils/factories/mockUserInput')
 
 
 describe('Testing Model', ()=>{
     describe('Testing findByEmail', ()=>{
-        it('should be successful', async ()=>{
+        it('should be successful when finding a user with email', async ()=>{
             const email = 'test1@gmail.com'
 
             const expectedResult = mockUser({
@@ -33,7 +34,7 @@ describe('Testing Model', ()=>{
     })
 
     describe('Testing findByUsername', () =>{
-        it('should be successful', async()=>{
+        it('should be successful to find a user by username', async()=>{
             const username = 'test'
 
             const expectedResult = mockUser({
@@ -47,6 +48,38 @@ describe('Testing Model', ()=>{
                 user_name:'test',
                 email:'test1@gmail.com'
             }))
+        })
+    })
+
+    describe('Testing Create User', () =>{
+        it('should be successful when creating an account', async()=>{
+            const timeNow = Date.now()
+
+            const customInput = {
+                email: `${timeNow}@test.com`,
+                username: timeNow
+            }
+
+            const userInput = mockUserInput(customInput)
+
+            createUser.mockResolvedValue(mockCreateUserRes())
+
+            await expect(createUser(userInput)).resolves.toEqual(expect.objectContaining({
+                email: `${timeNow}@test.com`,
+                user_name: timeNow
+            }))
+        })
+
+        it('should fail to register when email is already taken', async() =>{
+            const duplicateEmail = 'test1@gmail.com'
+
+            await expect(createUser(mockUserInput({email: duplicateEmail}))).toThrow() //can add specific error code from database
+        })
+
+        it('should fail to register when user is already taken', async() =>{
+            const duplicateUsername = 'test'
+
+            await expect(createUser(mockUserInput({email: duplicateUsername}))).toThrow() //can add specific error code from database
         })
     })
 })
