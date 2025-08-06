f982aa3426c437d8dcdc456340f60e9c476c18c6
kisungc <chung.kisung0@gmail.com>
2025-08-06 15:15:39 -0400

    completed integration test

--- tests/integration/auth.routes.test.js
+++ tests/integration/auth.routes.test.js
@@ -1,22 +1,73 @@
 const request = require("supertest")
 const app = require("../../app")
 
-describe('POST /auth/signup', ()=>{
-    it('should return 400 if email is missing',async()=>{
-        const uniqueValue = Date.now()
+describe('POST /auth/signup', () => {
+    it('should be return 201', async () => {
+        const uniqueValue = Date.now().toString()
         const res = await request(app)
-        .post('auth/signup')
-        .send({
-            password: 'ValidPass123!',
-            email:`${uniqueValue}@gmail.com`,
-            username: uniqueValue,
-            firstname: 'Test',
-            lastname: 'User',
-            skillLevel: 'Beginner'
-        })
+            .post('/auth/signup')
+            .send({
+                password: 'ValidPass123!',
+                email: `${uniqueValue}@gmail.com`,
+                username: uniqueValue,
+                firstname: 'Test',
+                lastname: 'User',
+                skillLevel: 'Beginner'
+            })
+
+        expect(res.statusCode).toBe(201)
+    })
+
+    it('should be return 400 if one of the field is missing', async () => {
+        const uniqueValue = Date.now().toString()
+        const res = await request(app)
+            .post('/auth/signup')
+            .send({
+                password: 'ValidPass123!',
+                email: ``,
+                username: uniqueValue,
+                firstname: 'Test',
+                lastname: 'User',
+                skillLevel: 'Beginner'
+            })
 
         expect(res.statusCode).toBe(400)
-        expect(res.body).toHaveProperty('error');
-        expect(res.body.error).toBe('One or more fields are missing');
+        expect(res.body).toHaveProperty('error')
+        expect(res.body.error).toBe('One or more fields are missing.');
+    })
+    it('should be return 409 if email is duplicate', async () => {
+        const uniqueValue = Date.now().toString()
+        const res = await request(app)
+            .post('/auth/signup')
+            .send({
+                password: 'ValidPass123!',
+                email: `chung.kisung0@gmail.com`,
+                username: uniqueValue,
+                firstname: 'Test',
+                lastname: 'User',
+                skillLevel: 'Beginner'
+            })
+
+        expect(res.statusCode).toBe(409)
+        expect(res.body).toHaveProperty('error')
+        expect(res.body.error).toBe('Email is already Taken.');
+    })
+    it('should be return 400 if there is an extra field in the req.body', async () => {
+        const uniqueValue = Date.now().toString()
+        const res = await request(app)
+            .post('/auth/signup')
+            .send({
+                password: 'ValidPass123!',
+                email: `chung.kisung0@gmail.com`,
+                username: uniqueValue,
+                firstname: 'Test',
+                lastname: 'User',
+                skillLevel: 'Beginner',
+                extraField: "extra"
+            })
+
+        expect(res.statusCode).toBe(400)
+        expect(res.body).toHaveProperty('error')
+        expect(res.body.error).toBe('Unexpected fields: extraField');
     })
 })
